04c5ce933eb28b3ee5f9d59115dcb27328074670
0codezone <mohittailor997@gmail.com>
2023-12-15 15:45:13 +0530

    Refactor todoController and todoModel by adding description, completed doc task

--- backend/src/controllers/todoController.js
+++ backend/src/controllers/todoController.js
@@ -1,24 +1,26 @@
 import Todo from "../models/todoModel.js";
 
-// create a todo
+// Create a todo
 export const createTodo = async (req, res) => {
   try {
-    const { title } = req.body;
-    const newTodo = new Todo({ title });
+    const { title, description, completed } = req.body; // Destructure additional fields if needed
+    const newTodo = new Todo({ title, description, completed });
     const savedTodo = await newTodo.save();
     res.status(201).json(savedTodo);
   } catch (error) {
-    res.status(500).json({ error: "createTodo failed internal server error" });
+    console.error(error);
+    res.status(500).json({ error: "not created Internal Server Error" });
   }
 };
 
-//get all todos
+// Get all todos
 export const getTodos = async (req, res) => {
   try {
     const todos = await Todo.find();
     res.status(200).json(todos);
   } catch (error) {
-    res.status(500).json({ error: "gettodo failed internal server error" });
+    console.error(error);
+    res.status(500).json({ error: "not shows alltodos Internal Server Error" });
   }
 };
 
@@ -32,17 +34,17 @@ export const getTodoById = async (req, res) => {
     res.json(todo);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: "Internal Server Error" });
+    res.status(500).json({ error: "not shpw todo Internal Server Error" });
   }
 };
 
 // Update a todo by ID
 export const updateTodoById = async (req, res) => {
   try {
-    const { title } = req.body;
+    const { title, description, completed } = req.body; // Destructure additional fields if needed
     const updatedTodo = await Todo.findByIdAndUpdate(
       req.params.id,
-      { title },
+      { title, description, completed },
       { new: true }
     );
     if (!updatedTodo) {
@@ -51,7 +53,7 @@ export const updateTodoById = async (req, res) => {
     res.json(updatedTodo);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: "Internal Server Error" });
+    res.status(500).json({ error: "not updated Internal Server Error" });
   }
 };
 
@@ -65,6 +67,6 @@ export const deleteTodoById = async (req, res) => {
     res.json(deletedTodo);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: "Internal Server Error" });
+    res.status(500).json({ error: "not delted Internal Server Error" });
   }
 };
--- backend/src/models/todoModel.js
+++ backend/src/models/todoModel.js
@@ -1,11 +1,26 @@
 import mongoose from "mongoose";
 
-const todoSchema = mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const todoSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
-});
+  { timestamps: true }
+);
 
 const Todo = mongoose.model("Todo", todoSchema);
 
